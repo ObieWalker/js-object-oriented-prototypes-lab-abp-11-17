7f340d42af9a37b2a2cf81126ec7801721ae4bb0
Obi <obinnawalker@gmail.com>
2017-11-10 13:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
-BoardMember.prototype.veto = function(){
+boardMembers.prototype.veto = function(){
     return 'No, I must disagree'
   }
 BoardMember.prototype..approve = function(){
